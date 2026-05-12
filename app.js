a7c7ac55ab812563af80cfeb403dcfd565771eb2
marvecodeit <marvellousonyema32@gmail.com>
2026-05-12 01:29:55 -0700

    updated backend

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const app = express();
 
 //Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://marveltechhub.vercel.app/',
+  origin: process.env.CLIENT_URL || 'https://marveltechhub.vercel.app',
   credentials: true
-}));
+}))
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes)
